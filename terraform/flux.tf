d473e3f121b629f87d331f5e5cc9557432b07d22
James Kelly <jamesdkelly88@outlook.com>
2025-01-18 21:51:04 +0000

    changing folder structure

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -1,7 +1,7 @@
 resource "flux_bootstrap_git" "this" {
   count              = local.clusters[var.cluster].fluxcd == true ? 1 : 0
   embedded_manifests = true
-  path               = "clusters/${var.cluster}/fluxcd.yaml"
+  path               = "clusters/${var.cluster}/flux"
 
   depends_on = [ time_sleep.wait_for_cluster ]
 }
