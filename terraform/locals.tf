4d81664bd7e4c5c838bcfbc146f00bce4885ec50
James Kelly <jamesdkelly88@outlook.com>
2026-05-06 16:04:36 +0100

    adding longhorn

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,6 +4,7 @@ locals {
   ip             = split("/", local.vm.primary_ip4)[0]
   context        = jsondecode(local.vm.local_context_data)
   bws_project_id = "15e59f8f-30ab-4415-bf66-b31c007af2e6"
+  schematic_id   = "613e1592b2da41ae5e265e8789429f22e121aab91cb4deb6bc3c0b6262961245"
 
   cluster = {
     host                   = data.talos_machine_configuration.this.cluster_endpoint
--- terraform/patches.tf
+++ terraform/patches.tf
@@ -1,18 +1,21 @@
 locals {
   patches = [
+    # install disk
     yamlencode({
       machine = {
         install = {
           disk  = "/dev/${local.context.install_disk}"
-          image = "ghcr.io/siderolabs/installer:v${local.context.talos_version}" # https://github.com/siderolabs/terraform-provider-talos/issues/272
+          image = "factory.talos.dev/metal-installer/${local.schematic_id}:v${local.context.talos_version}" # https://github.com/siderolabs/terraform-provider-talos/issues/272
         }
       }
     }),
+    # single node
     yamlencode({
       cluster = {
         allowSchedulingOnControlPlanes = true
       }
     }),
+    # metalLB
     yamlencode({
       cluster = {
         proxy = {
@@ -22,6 +25,52 @@ locals {
         }
       }
     }),
+    # extensions
+    yamlencode({
+      machine = {
+        install = {
+          extensions = [
+            {
+              image = "ghcr.io/siderolabs/iscsi-tools:v0.2.0"
+            },
+            {
+              image = "siderolabs/util-linux-tools:2.41.2"
+            }
+          ]
+        }
+      }
+    }),
+    # longhorn
+    yamlencode({
+      machine = {
+        disks = [
+          {
+            device = "/dev/${local.context.volume_disk}"
+            partitions = [
+              {
+                mountpoint = "/var/lib/longhorn"
+                size       = 0 # Use entire disk
+              }
+            ]
+          }
+        ]
+        kubelet = {
+          extraMounts = [
+            {
+              destination = "/var/lib/longhorn"
+              type        = "bind"
+              source      = "/var/lib/longhorn"
+              options = [
+                "bind",
+                "rshared",
+                "rw"
+              ]
+            }
+          ]
+        }
+      }
+    }),
+    # legacy version support
     yamlencode({
       machine = {
         install = {
